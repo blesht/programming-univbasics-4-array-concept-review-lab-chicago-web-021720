8d7bccba017391f26b8822fae413f35a06f13205
bryan lesht <bryan.lesht@colorado.edu>
2020-01-23 22:14:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -32,6 +32,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
- array.sort!
- array[0]
+ x = infinity 
+ array.length.times do |index|
+   if array[index] < x 
+     x = array[index]
+   end
+ end
+ x 
+ 
 end
