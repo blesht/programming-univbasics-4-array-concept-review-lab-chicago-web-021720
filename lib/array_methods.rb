4abee24d312c132a3441caff0314bcda6e3e53d5
bryan lesht <bryan.lesht@colorado.edu>
2020-01-23 21:18:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,14 +1,5 @@
 def find_element_index(array, value_to_find)
- i = array.length 
- while array[i] do 
-   if array[i]==value_to_find
-     i 
-     
-   else
-     nil 
-   end
-   i +=1 
- end
+
 end
 
 def find_max_value(array)
