fcd8a7571d6e29e89da4a7162e498f1c8aa9202a
bryan lesht <bryan.lesht@colorado.edu>
2020-01-23 21:55:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,13 @@
 def find_element_index(array, value_to_find)
 count = 0
 while count < array.length do
-array.index(value_to_find)
+array.index(value_to_find) = valuefound
 count +=1
 end
+if valuefound != nil 
+  valuefound
+else
+  nil 
 
 end
 
